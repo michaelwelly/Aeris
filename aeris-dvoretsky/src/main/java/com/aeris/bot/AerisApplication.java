89031f816e829314dbb2c5e335cf0d364cb45f29
Michael_Poedinenko <michael.poedinenko.mxr@gmail.com>
2024-12-07 19:09:41 +0500

    Update README.md

---
+++ aeris-dvoretsky/src/main/java/com/aeris/bot/AerisApplication.java
@@ -0,0 +1,11 @@
+package com.aeris.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AerisApplication.class, args);
+    }
+}
